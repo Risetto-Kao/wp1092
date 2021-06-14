0627aaf775a219251f18098b529e70528061ba12
Risetto-Kao <max52107429@gmail.com>
2021-06-14 23:23:16 +0800

    2021.06.14

--- hw8/own/frontend/src/App.js
+++ hw8/own/frontend/src/App.js
@@ -1,25 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
+import react from 'react';
+import { useState, useEffect } from 'react';
+import ChatRoomPage from './Containers/ChatRoomPage';
+import SingInPage from './Containers/SignInPage';
+import {message} from 'antd';
 
-function App() {
+
+const LOCALSTORAGE_KEY = 'saveUserName';
+
+const displayStatus = (payload) => {
+  if (payload.msg) {
+    const { type, msg } = payload;
+    const content = {
+      content: msg, duration: 1.5,
+    }
+    switch (type) {
+      case 'success':
+        message.success(content);
+        break;
+      case 'error':
+      default:
+        message.error(content);
+        break;
+    }
+  }
+}
+
+const App = () => {
+
+  const saveUserName = localStorage.getItem(LOCALSTORAGE_KEY);
+  
+
+  const [signedIn, setSignedIn] = useState(false);
+  const [userName, setUserName] = useState(saveUserName||'');
+
+
+
+  useEffect(()=>{
+    if(signedIn) localStorage.setItem(LOCALSTORAGE_KEY,userName);
+  },[signedIn]);
+  
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {signedIn ?
+        <ChatRoomPage userName={userName} /> :
+        <SingInPage
+          userName={userName}
+          setSignedIn={setSignedIn}
+          setUserName={setUserName}
+          displayStatus={displayStatus} />}
     </div>
   );
 }
-
+export {displayStatus};
 export default App;
